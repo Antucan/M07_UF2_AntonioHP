ace20e2f517993b6f6372c03bc62be12abe0c95a
Antucan <antherpra@gmail.com>
2025-01-31 19:07:46 +0100

    i forgot counter

--- resources/views/films/counter.blade.php
+++ resources/views/films/counter.blade.php
@@ -1,4 +1,15 @@
+@extends('layout.master')
+@section('header')
+    @parent
+@endsection
+
+@section('content')
 <h1>{{ $title }}</h1>
 <div align="center">
     <h3>{{ $count }}</h3>
 </div>
+@endsection
+
+@section('footer')
+    @parent
+@endsection
