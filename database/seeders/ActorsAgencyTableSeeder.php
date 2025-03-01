5e2fbd402ba7dfcda7135f33e826ccbe0f367184
Antucan <pl2023269@365.stucom.com>
2025-03-01 11:32:34 +0100

    #5 Extending database with agency data

--- database/seeders/ActorsAgencyTableSeeder.php
+++ database/seeders/ActorsAgencyTableSeeder.php
@@ -2,28 +2,27 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 use Faker\Factory as Faker;
-use Illuminate\Support\Facades\DB;
 
-class ActorFakerSeeder extends Seeder
+class ActorsAgencyTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     * 
-     * @return void
      */
     public function run(): void
     {
         $faker = Faker::create();
 
-        foreach (range(1, 10) as $index) {
+        for($i = 0; $i < 10; $i++) {
             DB::table('actors')->insert([
                 'name' => $faker->name,
                 'surname' => $faker->lastName,
                 'birthdate' => $faker->date,
                 'country' => $faker->countryCode,
                 'img_url' => $faker->imageUrl(640, 480, 'actor', true),
+                'agency' => $faker->company,//adding agency
                 'created_at' => now(),
                 'updated_at' => now(),
             ]);
