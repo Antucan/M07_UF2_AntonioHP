6a0e26bfe1ed444377b5d8289dbca4a4dc470787
Antucan <antherpra@gmail.com>
2025-03-26 20:23:59 +0100

    FilmController and Film model to use ORM Eloquent

---
+++ app/Models/Film.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Film extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'year',
+        'genre',
+        'country',
+        'duration',
+        'img_url',
+        'created_at',
+        'updated_at'
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime'
+    ];
+}
